d1b18961d1fa09c198832a006eb100fa80b6ebc2
theesanh <anhnguyenck3@gmail.com>
2026-06-28 07:01:15 +0700

    fix: update home route

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const app = express();
 
-// Cấu hình cơ bản để đọc dữ liệu từ request (cho Controller hoạt động)
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Một Route cơ bản để test khi web lên mạng
+// Route trang chủ
 app.get('/', (req, res) => {
-    res.send('🚀 Chào mừng đến với SwagStore! Web đã lên mạng thành công!');
+    res.send('<h1 style="color: green; text-align: center; margin-top: 20%;">🚀 Chào mừng đến với SwagStore!<br>Web đã lên mạng thành công!</h1>');
 });
 
-// BẮT BUỘC CHO RENDER: Phải dùng process.env.PORT
+// Cổng chạy server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
